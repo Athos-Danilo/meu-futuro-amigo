893eb76213ef5b1ec68be076e788d91d0f854ca6
Athos Danilo <athosdanilomarques@gmail.com>
2025-12-07 20:15:54 -0300

    2ª do footer

--- js/footer.js
+++ js/footer.js
@@ -2,12 +2,20 @@
 async function carregarFooter() {
     const footer = document.getElementById('footer');
     if (!footer) return;
-    try {const response = await fetch('/components/footer.html');
-        const html await response.text();
+    try {
+        const response = await fetch('/components/footer.html');
+
+        if (!response.ok) {
+            throw new Error(`Erro HTTP! status: ${response.status}`);
+        }
+
+        const html = await response.text();
+
         footer.innerHTML = html;
 
-        inicializarLogicafooter();
     } catch (error) {
         console.error('Erro ao carregar o footer', error);
     }
 }
+
+document.addEventListener('DOMContentLoaded', carregarFooter);
